scala-ide/scala-worksheet

commit 8410014e1d55d40aed08a46abb7313f7d4b349f2

Marked some method with ``override`` and made ``EvalScript.evalDocument`` private.

org.scalaide.worksheet/src/org/scalaide/worksheet/handlers/EvalScript.scala
@@ -61,7 +61,7 @@ class EvalScript extends AbstractHandler with HasLogger {
     } getOrElse false
 
 
-  def evalDocument(scriptUnit: ScriptCompilationUnit, doc: IDocument): Either[EvaluationError, String] = {
+  private def evalDocument(scriptUnit: ScriptCompilationUnit, doc: IDocument): Either[EvaluationError, String] = {
     scriptUnit.scalaProject.withPresentationCompiler { compiler =>
       val source = scriptUnit.batchSourceFile(SourceInserter.stripRight(doc.get.toCharArray))
       compiler.withResponse[Unit] { compiler.askReload(List(source), _) } // just make sure it's loaded

org.scalaide.worksheet/src/org/scalaide/worksheet/ScriptCompilationUnit.scala
@@ -23,12 +23,12 @@ case class ScriptCompilationUnit(val workspaceFile: IFile) extends InteractiveCo
 
   private var document: Option[IDocument] = None
 
-  def file: AbstractFile = EclipseResource(workspaceFile)
+  override def file: AbstractFile = EclipseResource(workspaceFile)
 
-  def scalaProject = ScalaPlugin.plugin.asScalaProject(workspaceFile.getProject).get
+  override lazy val scalaProject = ScalaPlugin.plugin.asScalaProject(workspaceFile.getProject).get
 
   /** Return the compiler ScriptSourceFile corresponding to this unit. */
-  def sourceFile(contents: Array[Char]): ScriptSourceFile = {
+  override def sourceFile(contents: Array[Char]): ScriptSourceFile = {
     ScriptSourceFile.apply(file, contents)
   }
 
@@ -37,12 +37,12 @@ case class ScriptCompilationUnit(val workspaceFile: IFile) extends InteractiveCo
     new BatchSourceFile(file, contents)
   }
 
-  def exists(): Boolean = true
+  override def exists(): Boolean = true
 
-  def getContents: Array[Char] = document.map(_.get.toCharArray).getOrElse(file.toCharArray)
+  override def getContents: Array[Char] = document.map(_.get.toCharArray).getOrElse(file.toCharArray)
 
   /** no-op */
-  def scheduleReconcile(): Response[Unit] = {
+  override def scheduleReconcile(): Response[Unit] = {
     val r = new Response[Unit]
     r.set()
     r
@@ -53,7 +53,7 @@ case class ScriptCompilationUnit(val workspaceFile: IFile) extends InteractiveCo
     this
   }
 
-  def currentProblems: List[IProblem] = {
+  override def currentProblems: List[IProblem] = {
     scalaProject.withPresentationCompiler { pc =>
       pc.problemsOf(file)
     }(Nil)
@@ -62,7 +62,7 @@ case class ScriptCompilationUnit(val workspaceFile: IFile) extends InteractiveCo
   /** Reconcile the unit. Return all compilation errors.
    *  Blocks until the unit is type-checked.
    */
-  def reconcile(newContents: String): List[IProblem] =
+  override def reconcile(newContents: String): List[IProblem] =
     scalaProject.withPresentationCompiler { pc =>
       askReload(newContents.toCharArray)
       pc.problemsOf(file)

commit f4a85a5025eeb698984a65de9ecb0db1d4786c3e

Fixed project's build
My commit SHA-8410014e1d55d40aed08a46abb7313f7d4b349f2 broke the build.  The
reason is that ``EvalScript.evalDocument`` was not declared private for testing
purposes. Making it public again fixed the build.

org.scalaide.worksheet/src/org/scalaide/worksheet/handlers/EvalScript.scala
@@ -84,7 +84,7 @@ class EvalScript extends AbstractHandler with HasLogger {
     } getOrElse false
 
 
-  private def evalDocument(scriptUnit: ScriptCompilationUnit, doc: IDocument): Either[EvaluationError, String] = {
+  def evalDocument(scriptUnit: ScriptCompilationUnit, doc: IDocument): Either[EvaluationError, String] = {
     scriptUnit.scalaProject.withPresentationCompiler { compiler =>
       val source = scriptUnit.batchSourceFile(SourceInserter.stripRight(doc.get.toCharArray))
       compiler.withResponse[Unit] { compiler.askReload(List(source), _) } // just make sure it's loaded