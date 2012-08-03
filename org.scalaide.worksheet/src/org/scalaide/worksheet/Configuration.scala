7a0b4781bc2144f8b0f9181047eabf51b084c5d9
Mirco Dotta <mirco.dotta@typesafe.com>
2012-08-03 16:18:54 +0200

    Fix #28 - Store instrumented sources/binaries in hidden ``.worksheet`` folder inside the project
    It was rather surprising to see that ``$instrumented.scala`` sources where stored inside the
    Eclipse installation. While the generated binaries where placed in the project's output folder
    (with the risk of overwriting existing class files!).
    
    This has been fixed. Now, worksheet instrumented sources are generated in
    ``${PROJECT_ROOT}/.worksheet/src`` (and I dropped the ``$instrumend`` suffix, not useful in
    my opinion). While, binaries generated from the instrumented sources are places in
    ``${PROJECT_ROOT}/.worksheet/bin``.

---
+++ org.scalaide.worksheet/src/org/scalaide/worksheet/Configuration.scala
@@ -0,0 +1,81 @@
+package org.scalaide.worksheet
+
+import org.eclipse.core.runtime.Path
+import scala.tools.eclipse.ScalaProject
+import java.util.concurrent.atomic.AtomicReference
+import org.eclipse.core.runtime.IPath
+import org.eclipse.core.resources.IProject
+import java.io.CharArrayReader
+import java.io.ByteArrayInputStream
+import java.io.File
+import org.eclipse.core.resources.IFolder
+import java.io.FileWriter
+
+object Configuration {
+  private val RootFolder = new Path(".worksheet")
+  private val SrcFolder = RootFolder.append("src")
+  private val BinFolder = RootFolder.append("bin")
+
+  def apply(project: ScalaProject): Configuration =
+    new Configuration(project.underlying)
+}
+
+final class Configuration private (project: IProject) {
+  import Configuration._
+
+  @inline private def location = project.getLocation
+
+  lazy val rootFolder: File = obtainFolder(RootFolder)
+  lazy val srcFolder: File = {
+    rootFolder
+    obtainFolder(SrcFolder)
+  }
+  lazy val binFolder: File = {
+    rootFolder
+    obtainFolder(BinFolder)
+  }
+
+  private def obtainFolder(relativePath: IPath): File = {
+    val absolutePath = location.append(relativePath)
+    val folder = createFolder(absolutePath)
+    folder
+  }
+
+  private def createFolder(absolutePath: IPath): File = create(absolutePath) { folder =>
+    folder.mkdirs() // FIXME: This is not really safe
+    assert(folder.isDirectory())
+  }
+
+  private def createFile(absolutePath: IPath): File = create(absolutePath) { file =>
+    file.createNewFile() // FIXME: This is not really safe
+    assert(file.isFile())
+  }
+
+  private def create(absolutePath: IPath)(f: File => Unit): File = {
+    val file = new File(absolutePath.toOSString())
+    f(file)
+    file
+  }
+
+  def clearSrcFolder(): Unit = clear(srcFolder)
+  def clearBinFolder(): Unit = clear(binFolder)
+
+  private def clear(folder: File): Unit = {
+    if (folder.isDirectory()) {
+      for (resource <- folder.listFiles())
+        resource.delete() // FIXME: This is not really safe
+    }
+  }
+
+  def touchSource(name: String, content: Array[Char] = Array[Char]()): File = {
+    val absolutePath = new Path(srcFolder.getAbsolutePath()).append(name)
+    val source = createFile(absolutePath)
+    assert(source.isFile())
+
+    val writer = new FileWriter(source)
+    try writer.write(content) // FIXME: what about the encoding?
+    finally writer.close() // FIXME: This is not really safe
+
+    source
+  }
+}
