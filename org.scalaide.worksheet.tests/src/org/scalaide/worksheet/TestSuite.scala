364b95e0a685b85f81a1244106e4d8e3a8bd28a8
Iulian Dragos <jaguarul@gmail.com>
2012-09-02 16:05:54 +0200

    Enabled auto-edit strategies tests.

--- org.scalaide.worksheet.tests/src/org/scalaide/worksheet/TestSuite.scala
+++ org.scalaide.worksheet.tests/src/org/scalaide/worksheet/TestSuite.scala
@@ -3,10 +3,12 @@ package org.scalaide.worksheet
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 import org.scalaide.worksheet.completion.CompletionTests
+import org.scalaide.worksheet.editor.EvaluationResultsAutoEditStrategyTest
 
 @RunWith(classOf[Suite])
 @Suite.SuiteClasses(Array(
   classOf[WorksheetEvalTest],
-  classOf[CompletionTests]
+  classOf[CompletionTests],
+  classOf[EvaluationResultsAutoEditStrategyTest]
 ))
 class TestSuite
