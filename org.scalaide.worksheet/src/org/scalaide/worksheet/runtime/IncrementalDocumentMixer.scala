991fd7c5544eb9a5e40910c82a2cd57d4df93fe4
Mirco Dotta <mirco.dotta@typesafe.com>
2012-10-16 13:48:55 +0200

    Fixes #93 - Editor cut-off value is now applied per-evaluation
    (cherry picked from f96fa3c6f8c59fa7f68fa247c365292ffe6ad35a)

--- org.scalaide.worksheet/src/org/scalaide/worksheet/runtime/IncrementalDocumentMixer.scala
+++ org.scalaide.worksheet/src/org/scalaide/worksheet/runtime/IncrementalDocumentMixer.scala
@@ -2,28 +2,30 @@ package org.scalaide.worksheet.runtime
 
 import java.io.Writer
 
-import scala.actors.{ Actor, DaemonActor, TIMEOUT }
+import scala.annotation.tailrec
+import scala.actors.{Actor, DaemonActor, TIMEOUT}
+import scala.collection.mutable.LinkedHashMap
+import scala.collection.mutable.ListBuffer
 import scala.tools.eclipse.logging.HasLogger
 
 import org.scalaide.worksheet.editor.DocumentHolder
 import org.scalaide.worksheet.text.{ Mixer, SourceInserter }
 
 object IncrementalDocumentMixer {
-  def apply(editor: DocumentHolder, source: Writer, maxOutput: Int = MaximumOutputChars): Actor = {
+  def apply(editor: DocumentHolder, source: Writer, maxOutput: Int): Actor = {
     val incrementalMixer = new IncrementalDocumentMixer(editor, source, maxOutput)
     incrementalMixer.start()
     incrementalMixer
   }
 
-  final val RefreshDocumentTimeout = 200
-  final val MaximumOutputChars = 100
+  private final val RefreshDocumentTimeout = 200
 
   /** How many ticks should pass with output not changing before adding a spinner? */
-  final val InfiniteLoopTicks = 1000 / RefreshDocumentTimeout // 1 sec
+  private final val InfiniteLoopTicks = 1000 / RefreshDocumentTimeout // 1 sec
 }
 
 private class IncrementalDocumentMixer private (editor: DocumentHolder, source: Writer, val maximumOutputSize: Int) extends DaemonActor with HasLogger {
-  import IncrementalDocumentMixer.{ RefreshDocumentTimeout, InfiniteLoopTicks }
+  import IncrementalDocumentMixer.{ RefreshDocumentTimeout, InfiniteLoopTicks}
 
   private val originalContent = editor.getContents
   private val stripped = SourceInserter.stripRight(editor.getContents.toCharArray)
@@ -45,7 +47,7 @@ private class IncrementalDocumentMixer private (editor: DocumentHolder, source: 
 
   private def updateDocument(newText: String): Unit = {
     if (newText.length > 0) {
-      val (mixed, lastInsertion) = mixer.mix(stripped, showLongRunning(pruneOutput(newText)).toCharArray())
+      val (mixed, lastInsertion) = mixer.mix(stripped, showLongRunning(pruneOutputPerEvaluation(newText)).toCharArray())
       editor.replaceWith(mixed.mkString, lastInsertion)
     }
   }
@@ -73,17 +75,38 @@ private class IncrementalDocumentMixer private (editor: DocumentHolder, source: 
     }
   }
 
-  def pruneOutput(newText: String): String = {
-    if (newText.length() > maximumOutputSize) {
-      val lastNL = newText.indexOf('\n', maximumOutputSize)
-      val endPos = if (lastNL == -1) newText.length else lastNL
-      val displayableText = newText.substring(0, endPos)
-      val suffix = if (displayableText.last == '\n') "" else "\n"
-      val lastLine = displayableText.lines.toSeq.last
-      val offset = lastLine.substring(0, lastLine.indexOf(' ', 0))
-      logger.debug("Cutting off large output at position: " + offset)
-      displayableText + suffix + offset + " Output exceeds cutoff limit. "
-    } else newText
+  def pruneOutputPerEvaluation(newText: String): String = {
+    // Groups together `lines` that share the same offset (lines order is maintained)   
+    def groupLinesByOffset(lines: List[String]): LinkedHashMap[String, ListBuffer[String]] = {
+      val offsetExtractor = """^(\d*)""".r
+      val groupedEvaluations = LinkedHashMap.empty[String, ListBuffer[String]]
+
+      for(line <- lines) offsetExtractor.findFirstIn(line) match {
+        case Some(offset) => 
+          groupedEvaluations.get(offset) match {
+            case None => groupedEvaluations += (offset -> ListBuffer(line))
+            case Some(result) => groupedEvaluations += ((offset, result += line))
+          }
+        case None =>
+          logger.error("Discarded evaluation result, this is a bug. Please open a ticket and make sure to provide the worksheet source that generated this warning.")
+      }
+
+      groupedEvaluations
+    }
+
+    val lines = newText.split('\n')
+    val linesByOffset = groupLinesByOffset(lines.toList)
+    
+    val evaluationResults = for((offset, evaluationOutputs) <- linesByOffset) yield {
+      val evaluationResult = evaluationOutputs.mkString("\n")
+      (if(evaluationResult.length <= maximumOutputSize) evaluationResult
+      else {
+        logger.debug("Cutting off large output at position: " + offset)
+        evaluationResult.take(maximumOutputSize) + '\n' + offset + " Output exceeds cutoff limit."
+      })
+    }
+    
+    evaluationResults.mkString("\n")
   }
 
   override def toString: String = "IncrementalDocumentMixer <actor>"
