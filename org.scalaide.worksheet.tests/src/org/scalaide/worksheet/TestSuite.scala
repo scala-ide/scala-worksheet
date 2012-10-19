3445d715af4924d12390afc813216a12e503b6d4
Mirco Dotta <mirco.dotta@typesafe.com>
2012-10-19 16:32:42 +0200

    Added a number of tests for instrumentation issues that have been fixed in the past month (cherry picked from commit f18b6e8965fb6c549a4c1d20fe12d0e833ec11e1)

--- org.scalaide.worksheet.tests/src/org/scalaide/worksheet/TestSuite.scala
+++ org.scalaide.worksheet.tests/src/org/scalaide/worksheet/TestSuite.scala
@@ -2,8 +2,10 @@ package org.scalaide.worksheet
 
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
+
 import org.scalaide.worksheet.completion.CompletionTests
 import org.scalaide.worksheet.editor.EvaluationResultsAutoEditStrategyTest
+import org.scalaide.worksheet.runtime.WorksheetEvalTest
 
 @RunWith(classOf[Suite])
 @Suite.SuiteClasses(Array(
